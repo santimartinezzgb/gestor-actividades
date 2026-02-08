91d263aeaffc0df5bef3cec3f69e60f1e1f0a369
santimartinezzgb <santimgb98@gmail.com>
2026-02-08 19:50:15 +0100

    Update

--- scripts/start-linux.sh
+++ scripts/start-linux.sh
@@ -5,10 +5,6 @@ echo "=================================="
 echo "  Iniciando Aplicación en  Linux  "
 echo "=================================="
 echo ""
-echo "Recuerda que el backend (Spring Boot) debe estar en ejecución."
-echo "Puedes iniciarlo en otra terminal con:"
-echo "cd ../backend-gestor-actividades && ./mvnw spring-boot:run"
-echo ""
 
 export ELECTRON_DISABLE_SANDBOX=1
 npm run electron
